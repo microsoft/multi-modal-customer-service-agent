cbf3cf6e258658b355ac6624bcc07a4a849af96d
Patrick O'Malley <20441551+pomalley08@users.noreply.github.com>
2024-12-13 16:54:36 -0500

    add 4o-mini option to intent detection logic

--- voice_agent/app/backend/utility.py
+++ voice_agent/app/backend/utility.py
@@ -45,7 +45,7 @@ async_client = AsyncAzureOpenAI(
 INTENT_SHIFT_API_KEY = os.environ.get("INTENT_SHIFT_API_KEY")
 INTENT_SHIFT_API_URL = os.environ.get("INTENT_SHIFT_API_URL") 
 INTENT_SHIFT_API_DEPLOYMENT=os.environ.get("INTENT_SHIFT_API_DEPLOYMENT")
-chat_deployment=os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT")
+AZURE_OPENAI_4O_MINI_DEPLOYMENT=os.environ.get("AZURE_OPENAI_4O_MINI_DEPLOYMENT")
   
 def allowSelfSignedHttps(allowed):  
     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):  
@@ -54,50 +54,64 @@ def allowSelfSignedHttps(allowed):
 allowSelfSignedHttps(True)  
   
 async def detect_intent(conversation): 
-    start_time = time.time() 
-    # Prepare the request data  
-# Format the data according to the ServiceInput schema  
-    value = f"{conversation}"  
-    data = {  
-        "input_data": {  
-            "columns": ["input_string"],  
-            "index": [0],  
-            "data": [[value]]  # Wrap value in a list to match the expected structure  
-        },  
-        "params": {}  
-    }  
-    
-    # Encode the data as JSON  
-    body = json.dumps(data).encode('utf-8')  
-    
-    # Check if the API key is provided  
-    if not INTENT_SHIFT_API_KEY:  
-        raise Exception("A key should be provided to invoke the endpoint")  
-    
-    # Set the headers  
-    headers = {  
-        'Content-Type': 'application/json',  
-        'Authorization': f'Bearer {INTENT_SHIFT_API_KEY}',  
-        'azureml-model-deployment': INTENT_SHIFT_API_DEPLOYMENT  
-    }  
-    
-    # Make the request  
-    req = urllib.request.Request(INTENT_SHIFT_API_URL, body, headers=headers)  
-  
-    
-    try:  
-        response = urllib.request.urlopen(req)  
-        result = response.read()
-        result = json.loads(result)[0]['0'].strip()
-        end_time = time.time()
-        print(f"Job succeeded in {end_time - start_time:.2f} seconds.")
-        return result
+    if INTENT_SHIFT_API_URL:
+        start_time = time.time() 
+        # Prepare the request data  
+        # Format the data according to the ServiceInput schema  
+        value = f"{conversation}"  
+        data = {  
+            "input_data": {  
+                "columns": ["input_string"],  
+                "index": [0],  
+                "data": [[value]]  # Wrap value in a list to match the expected structure  
+            },  
+            "params": {}  
+        }  
         
-    except urllib.error.HTTPError as error:  
-        print("The request failed with status code: " + str(error.code))  
-        print(error.info())  
-        print(error.read().decode("utf8", 'ignore'))  
-        return None  
+        # Encode the data as JSON  
+        body = json.dumps(data).encode('utf-8')  
+        
+        # Check if the API key is provided  
+        if not INTENT_SHIFT_API_KEY:  
+            raise Exception("A key should be provided to invoke the endpoint")  
+        
+        # Set the headers  
+        headers = {  
+            'Content-Type': 'application/json',  
+            'Authorization': f'Bearer {INTENT_SHIFT_API_KEY}',  
+            'azureml-model-deployment': INTENT_SHIFT_API_DEPLOYMENT  
+        }  
+        
+        # Make the request  
+        req = urllib.request.Request(INTENT_SHIFT_API_URL, body, headers=headers)  
+    
+        
+        try:  
+            response = urllib.request.urlopen(req)  
+            result = response.read()
+            result = json.loads(result)[0]['0'].strip()
+            end_time = time.time()
+            print(f"Job succeeded in {end_time - start_time:.2f} seconds.")
+            return result
+            
+        except urllib.error.HTTPError as error:  
+            print("The request failed with status code: " + str(error.code))  
+            print(error.info())  
+            print(error.read().decode("utf8", 'ignore'))  
+            return None 
+    else:
+        # fallback to gpt-4o-mini
+        messages = [
+            {"role": "system", "content": "You are a classifier model whose job is to classify the intent of the most recent user question into one of the following domains:\n\n- **hotel_agent**: Deal with hotel reservations, confirmations, changes, and general hotel policy questions.\n- **flight_agent**: Deal with flight reservations, confirmations, changes, and general airline policy questions.\n\nYou must only respond with the name of the predicted agent."},
+            {"role": "user", "content": conversation}
+        ]
+        response = await async_client.chat.completions.create(
+            model=AZURE_OPENAI_4O_MINI_DEPLOYMENT,
+            messages=messages,
+            max_tokens=20
+        )
+        intent = response.choices[0].message.content.strip()
+        return intent
 
 class SessionState:  
     def __init__(self): 
